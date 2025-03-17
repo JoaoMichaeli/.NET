5d4f542960576e526feebaf290dbcd3d2fe22413
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-03-17 12:00:44 -0300

    implementação da interface

---
+++ API/TDSPK.API/Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using TDSPK.API.Infrastructure.Persistence;
+using TDSPK.API.Migrations;
+
+namespace TDSPK.API.Infrastructure.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetByUserIdAsync();
+        Task AddAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+}
